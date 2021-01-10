adfb31e80cab8118aad852862b4516bc9663ce44
Vincent Vornkahl <if19b143@technikum-wien.at>
2021-01-10 19:58:45 +0100

    + hotfix Deck

--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -13,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DeckTest {
 
     List<Card> cards = new ArrayList<>();
+    Deck deck;
 
     @BeforeEach
     void setUp() {
@@ -36,46 +37,60 @@ public class DeckTest {
         cards.add(new Card("17","Deep Ocean Spell",90,CardType.Spell,ElementType.water));
         cards.add(new Card("18","Flame Spell",112,CardType.Spell,ElementType.fire));
         cards.add(new Card("19","Normal Magic Spell",100,CardType.Spell,ElementType.normal));
+        List<Card> tmp = new ArrayList<>();
+        Card card1 = cards.get((int)(Math.random() * cards.size()));
+        cards.remove(card1);
+        tmp.add(card1);
+        Card card2 = cards.get((int)(Math.random() * cards.size()));
+        cards.remove(card2);
+        tmp.add(card2);
+        Card card3 = cards.get((int)(Math.random() * cards.size()));
+        cards.remove(card3);
+        tmp.add(card3);
+        Card card4 = cards.get((int)(Math.random() * cards.size()));
+        cards.remove(card4);
+        tmp.add(card4);
+        deck = new Deck(tmp);
     }
 
     @Test
     public void deck_size() {
         List<Card> tmp = new ArrayList<>();
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
+        Card card1 = cards.get((int)(Math.random() * cards.size()));
+        cards.remove(card1);
+        tmp.add(card1);
+        Card card2 = cards.get((int)(Math.random() * cards.size()));
+        cards.remove(card2);
+        tmp.add(card2);
+        Card card3 = cards.get((int)(Math.random() * cards.size()));
+        cards.remove(card3);
+        tmp.add(card3);
+        Card card4 = cards.get((int)(Math.random() * cards.size()));
+        cards.remove(card4);
+        tmp.add(card4);
+        Card card5 = cards.get((int)(Math.random() * cards.size()));
+        cards.remove(card5);
+        tmp.add(card5);
         Deck deck1 = new Deck(tmp);
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        Deck deck2 = new Deck(tmp);
         assertEquals(4,deck1.getSize());
-        assertEquals(4,deck2.getSize());
     }
 
     @Test
     public void deck_remove() {
-        List<Card> tmp = new ArrayList<>();
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        Deck deck = new Deck(tmp);
         deck.removeCard(deck.getRandomCard());
         assertEquals(3,deck.getSize());
     }
 
     @Test
     public void deck_add() {
-        List<Card> tmp = new ArrayList<>();
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        tmp.add(cards.get((int)(Math.random() * cards.size())));
-        Deck deck = new Deck(tmp);
-        deck.addCard(cards.get((int)(Math.random() * cards.size())));
-        deck.addCard(cards.get((int)(Math.random() * cards.size())));
+        Card card5 = cards.get((int)(Math.random() * cards.size()));
+        cards.remove(card5);
+        Card card6 = cards.get((int)(Math.random() * cards.size()));
+        cards.remove(card6);
+
+        deck.addCard(card5);
+        deck.addCard(card6);
+        //deck.addCard(cards.get((int)(Math.random() * cards.size())));
         assertEquals(6,deck.getSize());
         deck.removeCard(deck.getRandomCard());
         assertEquals(5,deck.getSize());
